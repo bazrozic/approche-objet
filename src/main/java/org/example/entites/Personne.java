ff51f52bd02b269fdcf0f74026f6a3dabd87d972
basile <basilezic@gmail.com>
2025-01-16 14:38:00 +0100

    ajouter des choses

--- src/main/java/org/example/entites/Personne.java
+++ src/main/java/org/example/entites/Personne.java
@@ -6,6 +6,11 @@ public class Personne {
     private String prenom;
     private AdressePostale adressePostale;
 
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
     public Personne(String nom, String prenom, AdressePostale adressePostale){
         this.nom = nom;
         this.prenom = prenom;
