b57e2df6ece5358f462cd770cc766efa06314917
basile <basilezic@gmail.com>
2025-01-16 16:39:33 +0100

    tp numéro 5

---
+++ src/main/java/org/example/fr/epsi/banque/entites/Compte.java
@@ -0,0 +1,27 @@
+package org.example.fr.epsi.banque.entites;
+
+public class Compte {
+    private int numeroCompte;
+    private double soldeCompte;
+
+    public Compte(int numeroCompte, double soldeCompte) {
+        this.numeroCompte = numeroCompte;
+        this.soldeCompte = soldeCompte;
+    }
+
+    public int getNumeroCompte() {
+        return numeroCompte;
+    }
+
+    public void setNumeroCompte(int numeroCompte) {
+        this.numeroCompte = numeroCompte;
+    }
+
+    public double getSoldeCompte() {
+        return soldeCompte;
+    }
+
+    public void setSoldeCompte(double soldeCompte) {
+        this.soldeCompte = soldeCompte;
+    }
+}
